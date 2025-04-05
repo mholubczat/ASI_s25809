37074740cb2a0cdd01409c8ffd994b38587b9937
Maciej Hołubczat <s25809@pjwstk.edu.pl>
2025-04-05 11:29:18 +0200

    display predictions and model data

--- c2/learn.py
+++ c2/learn.py
@@ -1,6 +1,5 @@
-import autogluon
-from autogluon.tabular import TabularDataset, TabularPredictor
 import pandas as pd
+from autogluon.tabular import TabularDataset, TabularPredictor
 
 data1 = pd.read_csv('train.csv')
 data2 = pd.read_csv('test.csv')
@@ -8,4 +7,19 @@ data2 = pd.read_csv('test.csv')
 train_data = TabularDataset(data=data1)
 test_data = TabularDataset(data=data2)
 
-predictor = TabularPredictor(label='Survived').fit(train_data)
+predictor = (TabularPredictor(label='Survived', path='models', eval_metric='roc_auc')
+             .fit(train_data, presets='medium', time_limit=1800))
+predictor.save('models')
+
+predictions=predictor.predict(test_data)
+print(predictions)
+
+leaderboard = predictor.leaderboard()
+print(leaderboard)
+
+print(predictor.evaluate(train_data))
+
+model_name=predictor.model_best
+model_info=predictor.info()['model_info']
+hyperparams=model_info[model_name]['hyperparameters']
+print(hyperparams)
